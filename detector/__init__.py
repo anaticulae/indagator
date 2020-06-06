b64a6735959f9c38f5a8afa176a3c787a510411e
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-06-06 23:31:02 +0200

    0.1.1 automatically generated release

--- detector/__init__.py
+++ detector/__init__.py
@@ -12,7 +12,7 @@ import os
 # public title configuration
 from detector.feature.titlepage import RAWMAKER_CONFIGURATION
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
