294508c1c66d067a5e66679cbba4676c626eb687
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-11-21 16:29:19 +0100

    test(bib): verify bib extraction result

--- tests/bibliography/test_cli.py
+++ tests/bibliography/test_cli.py
@@ -54,7 +54,7 @@ def master75(flat):
 @pytest.mark.parametrize('source, pages, expected, validate', [
     pytest.param(power.link(power.BACHELOR056_PDF), '49:53', 32, None, id='bachelor56'),
     pytest.param(power.link(power.BACHELOR063_PDF), '59', 12, None, id='bachelor63'),
-    pytest.param(power.link(power.MASTER116_PDF), '97,98,99,100', 46, None, id='master116'),
+    pytest.param(power.link(power.MASTER116_PDF), '97,98,99,100', 46, None, id='master116'), # VALIDATED BY HAND
     pytest.param(power.link(power.MASTER089_PDF), None, 149, master89, id='master89'),
     pytest.param(power.link(power.BACHELOR090_PDF), '84:89', 52, bachelor90, id='bachelor90'),
     pytest.param(power.link(power.MASTER075_PDF), '70', 18, master75, id='master75'),
