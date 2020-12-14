9846d18143ea3be71a38169f9bab64b2651998ac
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-14 13:42:57 +0100

    style(format): format regex

--- detector/bibliography/label.py
+++ detector/bibliography/label.py
@@ -35,17 +35,17 @@ import utila
 # [ ]{0,3} Optional whitespaces
 
 TECHNICAL = r"""\[[ ]{0,3}
-                (?P<author>[\w\.]{2,3}[+]{0,1})[ ]{0,3}
-                (?P<year>\d{2})[ ]{0,3}
-                (?P<number>a|b|c|d){0,1}[ ]{0,3}
-                (\,[ ]{0,3}(Seite|S\.)[ ]{0,3}
-                (
-                 (?P<pagestart>\d{1,3})[ ]{0,3}\-[ ]{0,3}(?P<pageend>\d{1,3})|
-                 (?P<page>\d{1,3}[ ]{0,3}[f]{0,2}\.?)
-                )
-                ){0,1}
-                [ ]{0,3}\]
-             """
+            (?P<author>[\w\.]{2,3}[+]{0,1})[ ]{0,3}
+            (?P<year>\d{2})[ ]{0,3}
+            (?P<number>a|b|c|d){0,1}[ ]{0,3}
+            (\,[ ]{0,3}(Seite|S\.)[ ]{0,3}
+            (
+             (?P<pagestart>\d{1,3})[ ]{0,3}\-[ ]{0,3}(?P<pageend>\d{1,3})|
+             (?P<page>\d{1,3}[ ]{0,3}[f]{0,2}\.?)
+            )
+            ){0,1}[ ]{0,3}
+            \]
+"""
 
 
 def parses(content: str) -> iamraw.BibliographyReferences:
