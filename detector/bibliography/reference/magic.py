dc39c6b8efd1068f75e2084452782cc000c7549b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-05 23:26:12 +0100

    feat(bib): add very less accurate `magic` parser

---
+++ detector/bibliography/reference/magic.py
@@ -0,0 +1,60 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+"""\
+Agricola, Erhard (Hrg.), Wörter und Wendungen. Wörterbuch zum deutschen.
+        Sprachgebrauch, München: Max Hueber Verlag 1970.
+
+Bergsträsser, Gotthelf, Einführung in die semitischen Sprachen, München:
+        Max Hueber Verlag 1963.
+"""
+
+import re
+
+import german
+import iamraw
+
+
+def parse(raw: str) -> iamraw.BibliographyReference:
+    raw = raw.strip()
+    rest = raw
+    splitted = raw.split(',')
+    splitted = [item.strip() for item in splitted]
+    collected = []
+    for token in splitted:
+        if token.count(' ') <= 1:
+            collected.append(token)
+            continue
+        if '/' in token:
+            collected.append(token)
+            continue
+        break
+    collected: str = ', '.join(collected)
+
+    authors = german.authors(collected)
+    if authors == [['']]:
+        # HACK: remove empty authors
+        return None
+    authors = german.authors_decide(authors)
+
+    year_raw = re.search(r'(\d{4})\.$', raw)
+    year = int(year_raw[1]) if year_raw else None
+
+    rest = rest.replace(collected, '')
+    if year_raw:
+        rest = rest.replace(year_raw[0], '').strip()
+    title = rest.strip() if rest.strip() else None
+    if not title or not year:
+        return None
+    result = iamraw.BibliographyReference(
+        title=title,
+        authors=authors,
+        year=year,
+        raw=raw,
+    )
+    return result
