5fddb4ffaffe5074de7230161551c91ca1ca6b71
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-09 17:02:50 +0100

    feat(logging): decrease verbosity of logging

--- detector/bibliography/reference/tech.py
+++ detector/bibliography/reference/tech.py
@@ -25,7 +25,8 @@ def parse_single_row(content: str) -> iamraw.BibliographyReference:
         # Mostly a result of failure in layout grouping. This can
         # happen if a wrong layout grouping mechanism is used. This
         # issn't a problem cause we have more than one strategy.
-        utila.error(f'parses more than one reference: {content}')
+        utila.debug('parses more than one reference, '
+                    f'skip tech result: {content}')
         return None
     matched: iamraw.BibliographyReference = matched[0]
     content = content.replace(matched.raw, '')
