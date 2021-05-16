21681f8dc0625477240b075e2def28db34b0b124
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-16 13:27:47 +0200

    fix(bib): add szett as valid character

--- detector/bibliography/reference/freeand.py
+++ detector/bibliography/reference/freeand.py
@@ -55,7 +55,7 @@ MONTH = r"""
 # TODO: REMOVE 4,5 HACK: WHEN SUPPORTING HIGHNOTE
 AND = r"""
     (?P<authors>
-        [A-Z,;\(\)\.\-\&ÖÄÜ\d\' ]{5,}?
+        [A-Z,;\(\)\.\-\&ÖÄÜß\d\' ]{5,}?
         [^\(\)\[\]]                           # author does not ends with bracket
     )
     (
