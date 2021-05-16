3f2a2a9decaf49eddf4d214d801b7bdf739f713f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-16 20:30:13 +0200

    patch(remove): remove outdated patch

--- detector/__patch__.py
+++ detector/__patch__.py
@@ -6,14 +6,3 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-
-import utila
-
-
-# TODO: REMOVE LATER
-def simplify_chars(text: str) -> str:
-    text = text.replace('–', '-')
-    return text
-
-
-utila.simplify_chars = simplify_chars
