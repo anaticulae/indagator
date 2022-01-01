a89b688b0eaa1cdf1c7b460c33252af00412acb6
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-01 19:15:59 +0100

    test(bib): validate diss172

--- tests/bibliography/test_validate.py
+++ tests/bibliography/test_validate.py
@@ -121,6 +121,7 @@ def master148(flat):
     pytest.param(power.DISS143_PDF, '131', None, 'diss143p131', id='diss143p131'),
     pytest.param(power.BACHELOR067_PDF, '63:66', None, 'bachelor067', id='bachelor067'),
     pytest.param(power.DISS167_PDF, '140:167', None, 'diss167', id='diss167'),
+    pytest.param(power.DISS172_PDF, '152:172', None, 'diss172', id='diss172'),
 ])
 # yapf:enable
 @utilatest.longrun
