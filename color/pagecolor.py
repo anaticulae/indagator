fb5ee2ec5e7c14d59a08a74565f87e9fe5a0733b
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-03-31 18:48:05 +0200

    refactor(remove): remove unused color
    + If required, use utila instead

--- color/pagecolor.py
+++ color/pagecolor.py
@@ -56,25 +56,3 @@ def histogram(
         reverse=True,
     )
     return result
-
-
-def rgb2int(red, green, blue) -> int:
-    """\
-    >>> rgb2int(255, 255, 255)
-    16777215
-    >>> int2rgb(rgb2int(244, 25, 139))
-    (244, 25, 139)
-    """
-    return red << 16 | green << 8 | blue
-
-
-def int2rgb(value) -> tuple:
-    """\
-    >>> int2rgb(16777215)
-    (255, 255, 255)
-    """
-    return (
-        255 & value >> 16,
-        255 & value >> 8,
-        255 & value,
-    )
