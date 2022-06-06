c2b5ab38782612953695607da92f443198d1d11f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-06 12:44:13 +0200

    test(title): use modern validator approach

--- tests/detector_/titlepage/test_validate.py
+++ tests/detector_/titlepage/test_validate.py
@@ -7,14 +7,25 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import dataclasses
+import functools
+
 import iamraw
 import power
 import pytest
 import serializeraw
+import utila
 import utilatest
 
 import detector.path
+import tests.conftest
 import tests.detector_
+
+ARCHIVE = utila.join(
+    detector.ROOT,
+    'tests/detector_/titlepage/expected',
+    exist=True,
+)
 
 
 def bachelor37(titlepage: iamraw.TitlePage):
@@ -162,3 +173,75 @@ def test_validate_titlepage_extractor(source, check, testdir, monkeypatch):
     source = detector.path.titlepage_detected(testdir.tmpdir)
     titlepage = serializeraw.load_titlepage(source)
     check(titlepage)
+
+
+RESOURCES = [
+    item[0] if isinstance(item, tuple) else item
+    for item in tests.conftest.RESOURCES
+]
+RESOURCES = [pytest.param(item, id=utila.file_name(item)) for item in RESOURCES]
+
+
+@utilatest.longrun
+@pytest.mark.parametrize('source', RESOURCES)
+def test_validate(
+    source,
+    testdir,
+    monkeypatch,
+):
+    TitleCompare(
+        source,
+        testdir,
+        monkeypatch,
+    ).evaluate()
+
+
+class TitleCompare(utilatest.BaseLiner):
+
+    def __init__(self, source, testdir, monkeypatch):
+        super().__init__(
+            program=functools.partial(
+                tests.detector_.run,
+                monkeypatch=monkeypatch,
+            ),
+            step='titlepage',
+            pages=':',
+            source=source,
+            workdir=testdir.tmpdir,
+            archive=ARCHIVE,
+            loader=serializeraw.load_titlepage,
+        )
+
+    def load(self) -> str:
+        path = detector.path.titlepage_detected(self.workdir)
+        loaded = self.loader(path)
+        return loaded
+
+    def raw(self, value) -> str:
+        if not value:
+            return 'NO TITLE PAGE'
+        author = ''
+        if value.author:
+            author = f'{value.author.title.name}    '
+            author += f'{value.author.firstname}    {value.author.name}    '
+            author += value.author.raw.replace('\n', ' ')
+        thesis = ''
+        if value.thesis:
+            thesis = f'{value.thesis.typ.name}    {value.thesis.title}'
+        examiner = [
+            f'{item.title.name}    {item.raw}'.replace('\n', ' ')
+            for item in value.examiner
+        ]
+        examiner: str = '\n                    '.join(examiner)
+        data = dict(
+            author=author,
+            title=value.title,
+            thesis=thesis,
+            date=value.date.raw if value.date else None,
+            matrikel=value.matrikel.number if value.matrikel else None,
+            examiner=examiner,
+        )
+        result = utila.dict_dump(data)
+        # remove trailing white spaces
+        result: str = '\n'.join(item.rstrip() for item in result.splitlines())
+        return result
