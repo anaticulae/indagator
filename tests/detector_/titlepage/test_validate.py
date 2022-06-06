7cd9142ea6acb8156ee77a57df7fa6af5e8e6325
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-06 12:49:17 +0200

    test(refactor): simplify code

--- tests/detector_/titlepage/test_validate.py
+++ tests/detector_/titlepage/test_validate.py
@@ -66,30 +66,34 @@ class TitleCompare(utilatest.BaseLiner):
         return loaded
 
     def raw(self, value) -> str:
-        if not value:
-            return 'NO TITLE PAGE'
-        author = ''
-        if value.author:
-            author = f'{value.author.title.name}    '
-            author += f'{value.author.firstname}    {value.author.name}    '
-            author += value.author.raw.replace('\n', ' ')
-        thesis = ''
-        if value.thesis:
-            thesis = f'{value.thesis.typ.name}    {value.thesis.title}'
-        examiner = [
-            f'{item.title.name}    {item.raw}'.replace('\n', ' ')
-            for item in value.examiner
-        ]
-        examiner: str = '\n                    '.join(examiner)
-        data = dict(
-            author=author,
-            title=value.title,
-            thesis=thesis,
-            date=value.date.raw if value.date else None,
-            matrikel=value.matrikel.number if value.matrikel else None,
-            examiner=examiner,
-        )
-        result = utila.dict_dump(data)
-        # remove trailing white spaces
-        result: str = '\n'.join(item.rstrip() for item in result.splitlines())
-        return result
+        return rawtitle(value)
+
+
+def rawtitle(value) -> str:
+    if not value:
+        return 'NO TITLE PAGE'
+    author = ''
+    if value.author:
+        author = f'{value.author.title.name}    '
+        author += f'{value.author.firstname}    {value.author.name}    '
+        author += value.author.raw.replace('\n', ' ')
+    thesis = ''
+    if value.thesis:
+        thesis = f'{value.thesis.typ.name}    {value.thesis.title}'
+    examiner = [
+        f'{item.title.name}    {item.raw}'.replace('\n', ' ')
+        for item in value.examiner
+    ]
+    examiner: str = '\n                    '.join(examiner)
+    data = dict(
+        author=author,
+        title=value.title,
+        thesis=thesis,
+        date=value.date.raw if value.date else None,
+        matrikel=value.matrikel.number if value.matrikel else None,
+        examiner=examiner,
+    )
+    result = utila.dict_dump(data)
+    # remove trailing white spaces
+    result: str = '\n'.join(item.rstrip() for item in result.splitlines())
+    return result
